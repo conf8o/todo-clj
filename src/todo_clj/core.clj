4dafb7552fb050b515651ca8775c8c350bddcaf4
conf8o <41929738+conf8o@users.noreply.github.com>
2021-05-29 16:12:35 +0900

    first commit

---
+++ src/todo_clj/core.clj
@@ -0,0 +1,27 @@
+(ns todo-clj.core
+  (:require [compojure.core :refer [routes]]
+            [ring.adapter.jetty :as server]
+            [todo-clj.handler.main :refer [main-routes]]
+            [todo-clj.handler.todo :refer [todo-routes]]))
+
+(defonce server (atom nil))
+
+(def app
+  (routes
+   todo-routes
+   main-routes))
+
+(defn start-server []
+  (when-not @server
+    (reset! server
+            (server/run-jetty #'app {:port 3000 :join? false}))))
+
+(defn stop-server []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
+(defn restart-server []
+  (when @server
+    (stop-server)
+    (start-server)))
